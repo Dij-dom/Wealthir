b649a95c3285b068da501cadfc5b58dd32e4ea60
sepp <jakeyjoey66@gmail.com>
2024-01-07 19:30:24 +0530

    First commit, server intialised

--- server.js
+++ server.js
@@ -1,28 +1,25 @@
 const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const dotenv = require('dotenv');
-const colors = require('colors');
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
 
-//config dot env file
-dotenv.config()
+dotenv.config();
 
-//rest object
 const app = express();
 
 //middlewares
 app.use(cors());
+app.use(express.json());
 app.use(morgan('dev'));
-app.use(cors());
 
 //routes
-app.get('/', (req, res) => {
-    res.send('<h1>Welcome to ExpenseXp<h1>');
-});
+app.get('/', (req, res) =>{
+    res.send(`<h1>Hello World</h1>`);
+})
 
-const PORT = 8080 || express.env.PORT;
+//port
+const PORT = 8080 || process.env.PORT;
 
-//listen server
-app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}...`);
+app.listen(PORT, () =>{
+    console.log(`Server listening on ${PORT} port`);
 });
