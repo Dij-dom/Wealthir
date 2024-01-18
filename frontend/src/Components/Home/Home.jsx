1f6b52774c94f94f702ee73d93a1e9d2226bc8d8
sepp <jakeyjoey66@gmail.com>
2024-01-18 17:22:49 +0530

    Income/Expense Records

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,84 +1,110 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Modal from 'react-modal';
 //import './home.css';
 import Header from './header';
 import Details from './details';
+import { useAuthContext } from '../../hooks/useAuthContext';
+import IncomeDetails from './incomeDetails';
 
 const Home = () => {
   const [isIncomeModalVisible, setIncomeModalVisible] = useState(false);
-  const [incomeList, setIncomeList] = useState([]);
-  const [title, setTitle] = useState('Income');
-  const [amount, setAmount] = useState('');
-  const [date, setDate] = useState(null);
+  const [transactions, setTransactions] = useState(null);
 
- 
+  const { user } = useAuthContext();
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      const response = await fetch('http://localhost:4000/api/transactions', {
+        headers: {
+          'Authorization': `Bearer ${user.token}`
+        }
+      });
+      const json = await response.json();
+
+      if (response.ok) {
+        setTransactions(json);
+      }
+    }
+
+    if (user) {
+      fetchTransactions();
+    }
+
+  }, [])
 
 
-
-  const showIncomeModal = () => {
-    setIncomeModalVisible(true);
-  };
-
-  const handleIncomeModalOk = () => {
-    const newIncome = {
-      title,
-      amount,
-      date: date ? date.toLocaleDateString() : '',
+  /*
+    const showIncomeModal = () => {
+      setIncomeModalVisible(true);
     };
-
-    setIncomeList([...incomeList, newIncome]);
-    setIncomeModalVisible(false);
-
-    setTitle('');
-    setAmount('');
-    setDate(null);
-  };
-
-  const handleIncomeModalCancel = () => {
-    setIncomeModalVisible(false);
-  };
-
+  
+    const handleIncomeModalOk = () => {
+      const newIncome = {
+        title,
+        amount,
+        date: date ? date.toLocaleDateString() : '',
+      };
+  
+      setIncomeList([...incomeList, newIncome]);
+      setIncomeModalVisible(false);
+  
+      setTitle('');
+      setAmount('');
+      setDate(null);
+    };
+  
+    const handleIncomeModalCancel = () => {
+      setIncomeModalVisible(false);
+    };
+  */
 
   return (
     <div className="home-container">
-      <Header/>
+      <Header />
       <div className="home-content">
-        <Details/>
+        <Details />
         <section className="home-section2">
           <div className="mainsecSearch">
-          <div className="mainsecSearchleft">
-            <i className="fas fa-filter fa-lg" style={{ color: '#808080' }}></i>
-          </div>
-          <div className="mainsecSearchmiddle">
-            <input type="text" placeholder="Search..." className="search-bar" />
-          </div>
-          <div className="mainsecSearchright">
-            <button className="add-button"id='income' onClick={showIncomeModal}>ADD INCOME</button>
-            <button className="add-button"id='expense'>ADD EXPENSE</button>
-            {/* <div className="dropdown-content">
+            <div className="mainsecSearchleft">
+              <i className="fas fa-filter fa-lg" style={{ color: '#808080' }}></i>
+            </div>
+            <div className="mainsecSearchmiddle">
+              <input type="text" placeholder="Search..." className="search-bar" />
+            </div>
+            <div className="mainsecSearchright">
+              <button className="add-button" id='income'>ADD INCOME</button>
+              <button className="add-button" id='expense'>ADD EXPENSE</button>
+              {/* <div className="dropdown-content">
               <p>INCOME</p>
               <p>EXPENSE</p>
             </div> */}
-          </div>
+            </div>
           </div>
           <div className="mainsecContent">
             <div className="mainsecContent-left">
               <h3>Income Records</h3>
               <div className="income-records">
-                {incomeList.map((income, index) => (
-                  <div key={index}>
-                    <p>Title: {income.title}</p>
-                    <p>Amount: {income.amount}</p>
-                    <p>Date: {income.date}</p>
-                  </div>
-                ))}
+              {transactions && transactions.map((transaction) => (
+            transaction.type === 'Income' && (
+                <IncomeDetails key={transactions._id} transaction={transaction} />
+                )))}
+  
               </div>
+
             </div>
             <div className="mainsecContent-right">
-            <h3>Expense Records</h3>
-          <div className="expense-records">
-            {/* Content for Expense Records goes here */}
-          </div>
+              <h3>Expense Records</h3>
+              <div className="expense-records">
+                {transactions && transactions.map((transaction) => (
+                  transaction.type === 'Expense' && (
+                    <div key={transaction._id} className='income-details'>
+                      <h4>{transaction.title}</h4>
+                      <p>Amount: {transaction.amount}</p>
+                      <p>Date: {transaction.date}</p>
+                      <p>Description: {transaction.description}</p>
+                    </div>
+                  )))}
+              </div>
             </div>
           </div>
 
@@ -89,7 +115,7 @@ const Home = () => {
         <p>&copy; 2024 Your Website. All rights reserved.</p>
       </footer>
 
-      {/* Income Modal */}
+      {/* Income Modal 
       <Modal
         isOpen={isIncomeModalVisible}
         onRequestClose={handleIncomeModalCancel}
@@ -134,7 +160,9 @@ const Home = () => {
         style={{ backgroundColor: '#f44336', color: 'white', padding: '10px 15px' }}  
         >Cancel</button>
       </Modal>
+      */}
     </div>
+
   );
 }
 
